48d2e6399633830072815cad7d83d5fbbfed3a61
cyao1234 <chengyin.yao@live.com>
2022-04-25 15:02:55 +0200

    More code ..... x4

--- wikitools.py
+++ wikitools.py
@@ -21,6 +21,7 @@ def main():
     tmp3 = re.search('\"aliases\": \\[ \"GI\" ],\\n			\"objclass\": \"InitialGridItemProperties\",\\n			\"objdata\": {([.\\n\\t\\s\\S]+?)} ]', file_data)
     tmp4 = re.search('\"FlagCount\": (\\d+?),', file_data)
     tmp5 = re.search('\"Waves\": \\[ (.+?) ]\\n', file_data)
+    tmp6 = re.search('\"WavesPerFlag\": (\d+)\\n', file_data)
     waves = re.sub("\\[|]", "|", re.sub("([^\\d])]", "\\1", re.sub(
         ",|:|Type:RTID|objclass|objdata|@ZombieTypes|Zombies|}|{|\\[{Row|aliases|Row", "",
         re.sub(".+\\[]}},{aliases:|]}}],version:1}", "",
@@ -45,6 +46,8 @@ def main():
         flag_count = tmp4.group(1)
     if tmp5:
         wave_format = tmp5.group(1)
+    if tmp6:
+        waves_per_flag = tmp6.group(1)
     before_level = level_name.split("-")
     before_level[0] = str(int(before_level[0]) - 1)
     before_level = "-".join(before_level)
@@ -66,10 +69,34 @@ def main():
     sorted_list.append(sorted_list[0])
     sorted_list.pop(0)
 
+    final_list = []
     for i in range(len(wave_format)):
-        for I in range(len(sorted_list)):
-            if I % 2 == 5:
-                print("h")
+        if 1 % 2 == 1:
+            for o in range(len(sorted_list)):
+                if o % 2 == 1:
+                    if int(wave_format[i - 1]) == int(sorted_list[o - 1]):
+                        final_list.append(sorted_list[o])
+
+    my = "".join(final_list[0][0])
+
+    final_string = ""
+
+    for item in range(len(final_list[0])):
+
+        if item == waves_per_flag:
+            tmp = "Flag"
+        else:
+            tmp = "-"
+
+        if item == len(final_list[0]):
+            tmp2 = ""
+        else:
+            tmp2 = "-"
+        final_string += f'|{item}\n'
+        final_string += '|' + "".join(final_list[0][item - 1][::2]) + "\n"
+        final_string += '|None\n'
+        final_string += f'|{tmp}\n'
+        final_string += f'|{tmp2}'
 
     # openUrl(f'https://project-eclise.fandom.com/wiki/{level_name}(Alpha)?action=edit')
     print(f'opened https://project-eclise.fandom.com/wiki/{level_name}(Alpha)?action=edit')
@@ -99,7 +126,8 @@ def main():
 !Non-dynamic zombies
 !Ambush zombies
 !Notes
-|-""".format(level_name, full_level_name, level_name, gravestone_count, flag_count, before_level, after_level, no_numb_lvl_name, level_name)
+|-
+{}""".format(level_name, full_level_name, level_name, gravestone_count, flag_count, before_level, after_level, no_numb_lvl_name, level_name, final_string)
 
     pyperclip.copy(final_string)
 
