ffa02127ae11a9ea7f5393190ad9c448fc1db8cc
cyao1234 <chengyin.yao@live.com>
2022-04-29 15:27:58 +0200

    made convert.txt's outline, now just need to copy it there

--- WikiToolsA.py
+++ WikiToolsA.py
@@ -103,10 +103,10 @@ def main():
 
     
     # For dist use:
-    openUrl(f'https://project-eclise.fandom.com/wiki/{level_name}(Alpha)?action=edit')
+    # openUrl(f'https://project-eclise.fandom.com/wiki/{level_name}(Alpha)?action=edit')
 
     # For debug use:
-    # print(f'opened https://project-eclise.fandom.com/wiki/{level_name}(Alpha)?action=edit')
+    print(f'opened https://project-eclise.fandom.com/wiki/{level_name}(Alpha)?action=edit')
 
     gravestone_count = gi.count("gravestone_egypt" or "gravestone_dark" or "gravestoneSunOnDestruction")
     no_numb_lvl_name = re.sub("\\d+?-\\d+: ", "", full_level_name)
