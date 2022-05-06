86c53bf0a9037d4969404ecca776cc5acdf111c6
cyao1234 <chengyin.yao@live.com>
2022-05-06 16:37:38 +0200

    done V2

--- WikiTools.py
+++ WikiTools.py
@@ -1,151 +1,161 @@
+from tkinter import *
 import json
 import re
-from convert import convert
-import functions as f
+import itertools
+import functions as fun
 
 
-def main(file_path: str):
-    print(f"Transforming ~/{file_path}")
+def convert(file):
+    f = open(file, 'r')
+    json_file = json.load(f)
 
-    data = open("data.json", "r")
-    j_d = json.load(data)
+    template = ''.join(open('template.txt', 'r').readlines()[7:])
+    link = ''.join(open('template.txt', 'r').readlines()[1:2])
 
-    file_data = open(file_path, "r").read()
-    level_name_tmp = re.search('\"Name\": \"(\\d+-\\d+).+\",\n', file_data)
-    full_level_name_tmp = re.search('\"Name\": \"(\\d+-\\d+.+)\",\n', file_data)
-    gi_tmp = re.search(
-        '\"aliases\": \\[ \"GI\" ],\\n			\"objclass\": \"InitialGridItemProperties\",\\n			\"objdata\": {([.\\n\\t\\s\\S]+?)} ]',
-        file_data)
-    flag_count_tmp = re.search('\"FlagCount\": (\\d+?),', file_data)
-    wave_format_tmp = re.search('\"Waves\": \\[ (.+?) ]\\n', file_data)
-    waves_tmp = re.sub(', {\\n\\t+\\"aliases\\": \\[ \"RM\" ][\\s\\S]+} ', ' ', re.sub('\"DinoWaveDuration\": \"\\d\"', '', file_data))
-    waves_tmp2 = re.search('({\\n\\s+\"aliases\": \\[ \"Wave1\" ],\n[\\s|\\S]+\"*\\n\\t+} ]),*\n', waves_tmp).group(1)
-    waves = re.sub("\\[|]", "|", re.sub("([^\\d])]", "\\1", re.sub(
-        ",|:|Type:RTID|objclass|objdata|@ZombieTypes|Zombies|}|{|\\[{Row|aliases|Row|DinoWaveActionProps|DinoType:RTID|DinoRow|DinoWaveDuration|Dino|Type",
-        "",
-        re.sub(".+\\[]}},{aliases:|]}}],version:1}", "",
-               re.sub("\\t|\\n| |\"|SpawnZombiesJitteredWaveActionProps|DinoWaveActionProps", "", waves_tmp2))))).split("|")
-    waves.remove("")
+    name = json_file['objects'][0]['objdata']['Name']
+    name_number = json_file['#comment']
+    first_reward = json_file['objects'][0]['objdata']['FirstRewardParam'].capitalize()
+    if first_reward == "big_moneybag":
+        first_reward = ""
+    replay_reward = ''
 
-    gi = ''
-    level_name = ''
-    if level_name_tmp:
-        level_name = level_name_tmp.group(1)
-    if full_level_name_tmp:
-        full_level_name = full_level_name_tmp.group(1)
-    if gi_tmp:
-        gi = gi_tmp.group(1)
-    if flag_count_tmp:
-        flag_count = flag_count_tmp.group(1)
-    if wave_format_tmp:
-        wave_format = wave_format_tmp.group(1)
+    first = int(name_number.split('-')[0])
+    second = int(name_number.split('-')[1])
 
-    before_level = level_name.split("-")
-    before_level[1] = str(int(before_level[1]) - 1)
-    before_level = "-".join(before_level)
-    after_level = level_name.split("-")
-    after_level[1] = str(int(after_level[1]) + 1)
-    after_level = "-".join(after_level)
+    print(first, second, str(first) + '-' + str(second - 1))
 
-    new_waves = []
-    for index in range(len(waves)):
-        index += 1
-        if index % 2 == 0:
-            new_waves.append(re.sub('(\\d);({{P\\|\\S+\\s*\\S*\\|2}})<sup>0</sup>', '\\2<sup>\\1</sup>',
-                                    re.sub('|AdditionalPlantfood\\d', '',
-                                           re.sub("(<sup>\\d</sup>);(\\d+\\.*\\d*);({{P\\|.+?\\|2}})", "\\3\\1#\\2#",
-                                                  re.sub("<sup>(\\d)</sup>;<sup>0</sup>", "<sup>\\1</sup>", convert(
-                                                      re.sub('\"AdditionalPlantfood\": 1,', '',
-                                                             re.sub("(\\d)\\(", "<sup>\\1</sup>(",
-                                                                    str(waves[index - 1]))))))))[:-1].split("#"))
+    if second - 1 == 0 or second - 1 == 10 or second - 1 == 20:
+        if second == 1:
+            before_name = 'Null'
+        elif first == 1:
+            before_name = str(first + 11) + '-' + str(second - 10)
         else:
-            new_waves.append(re.sub("Wave", "", str(waves[index - 1])))
+            before_name = str(first - 1) + '-' + str(second + 9)
+    else:
+        before_name = str(first) + '-' + str(second - 1)
 
-    tmp_list = f.tmp_list(new_waves)
+    if second + 1 == 10 or second + 1 == 20 or second + 1 == 30:
+        after_name = str(first + 1) + '-' + str(second - 8)
+    else:
+        after_name = str(first) + '-' + str(second + 1)
 
-    for i in range(len(tmp_list)):
-        tmp_list[i - 1] = re.sub('<sup>\\d</sup>', "", tmp_list[i - 1])
+    json_file_removed = json_file['objects']
+    json_file_removed.remove(json_file_removed[0])
 
-    zmb_set = set(tmp_list)
-    wave_format = re.sub('RTID|@CurrentLevel|\\)|\\(|Wave|]|\\[|\"| |\\n', "", wave_format).split(',')
+    wave_list = []
+    for item in json_file_removed:
+        if item['aliases'] == ["WaveManager"]:
+            for i in item['objdata']["Waves"]:
+                wave_list.append([re.sub('RTID\\(([\s\S]+?)@CurrentLevel\\)', '\\1', i[0])])
 
-    # Sorts the zombies
-    sorted_list = f.sort_zombies(new_waves)
+    flags = 1
+    waves_per_flag = 10
+    wave_count = 10
+    for item in json_file_removed:
+        if item['aliases'] == ["WaveManager"]:
+            wave_count = item['objdata']["WaveCount"]
+            waves_per_flag = item['objdata']["FlagWaveInterval"]
+            flags = wave_count // waves_per_flag
 
-    final_list = []
-    for i in range(len(wave_format)):
-        for o in range(len(sorted_list)):
-            if o % 2 == 1:
-                if wave_format[i - 1] == sorted_list[o - 1]:
-                    final_list.append(sorted_list[o])
+    num_items_on_map = 0
+    try:
+        for i in json_file_removed:
+            if i['aliases'] == ['GI']:
+                num_items_on_map = len(i['objdata']['InitialGridItemPlacements'])
+    except KeyError:
+        num_items_on_map = 0
 
-    final_list.append(final_list[0])
-    final_list.pop(0)
+    plants = []
+    try:
+        for i in json_file_removed:
+            if i['aliases'] == ["SeedBank"]:
+                for plant in i['objdata']['PresetPlantList']:
+                    plants.append(plant['PlantType'])
+                plants = ', '.join(plants)
+    except KeyError:
+        plants = 'Chooser'
 
-    final_string = ""
-    for item in range(len(final_list)):
-        if ((item + 1) / int(re.search('\"WavesPerFlag\": (\\d+),\\n', file_data).group(1)) % 1) == 0.0:
-            tmp = "Flag"
-        else:
-            tmp = "<br />"
+    unsorted_list = []
 
-        if item == len(final_list) - 1:
-            tmp2 = "}"
-        else:
-            tmp2 = "-"
+    for i, item in itertools.product(wave_list, json_file_removed):
+        item_val = item['aliases']
+        if item_val == i:
+            unsorted_list.append(i[0])
+            tmp_list = []
+            for zombie in item['objdata']['Zombies']:
+                converted_zombie = fun.convert(re.sub('RTID\\(([\s\S]+?)@ZombieTypes\\)', '(\\1)', zombie['Type']))
+                try:
+                    row = zombie['Row']
+                except KeyError:
+                    row = '0'
 
-        final_string += f'|{item + 1}\n|'
-        for i in range(len(final_list[item])):
-            final_string += f'{final_list[item][i][0]} '
-        if ((item + 1) / int(re.search('\"WavesPerFlag\": (\\d+),\\n', file_data).group(1)) % 1) == 0.0:
-            final_string += '{{P|Flag Zombie|2}}'
-        final_string += '\n|None\n'
-        final_string += f'|{tmp}\n'
-        final_string += f'|{tmp2}\n'
+                tmp_list2 = f'{converted_zombie}<sup>{row}</sup> '.split(";")
+                tmp_list.append(tmp_list2[1])
+                tmp_list.append(tmp_list2[0])
+            unsorted_list.append(tmp_list)
 
-    # For dist use:
-    f.openUrl(j_d["link"].format(level_name))
+    sorted_list = fun.sort_zombies(unsorted_list)
 
-    # For debug use:
-    # print(f'opened https://project-eclise.fandom.com/wiki/{level_name}(Alpha)?action=edit')
+    zombie_waves = ''
+    tmp_wave = 0
+    for i in range(len(sorted_list)):
+        if i % 2 != 0:
+            if re.match(r'Wave\d', sorted_list[i - 1]):
+                tmp_wave += 1
+                tmp = sorted_list[i][::2]
+                tmp = ' '.join(tmp)
+                zombie_waves += f'|{tmp_wave}\n|{tmp}'
 
-    gravestone_count = gi.count("gravestone_egypt" or "gravestone_dark" or "gravestoneSunOnDestruction")
-    no_numb_lvl_name = re.sub("\\d+?-\\d+: ", "", full_level_name)
+                if tmp_wave % waves_per_flag == 0:
+                    zombie_waves += '{{P|Flag Zombie|2}}\n'
+                else:
+                    zombie_waves += '\n'
+                zombie_waves += '|None\n'
 
-    final_string = """{{{{BAHTabber|Level={}}}}}
-{{{{Level Infobox
-|name = {}
-|image = {}Alpha.jpg
-|Type = Regular
-|EM = {}
-|Flag = {}
-|Plant = Choice
-|Zombie = {}
-|FR =
-|NR =
-|before = {} (Alpha)
-|after = {} (Alpha)
-}}}}
-\'\'\'{}\'\'\', also known as \'\'\'{}\'\'\'
+                if tmp_wave % waves_per_flag == 0:
+                    zombie_waves += '|Flag\n'
+                else:
+                    zombie_waves += '|<br />\n'
 
-{{{{clear}}}}
-== Waves ==
-{{| class="article-table"
-!Waves
-!Non-dynamic zombies
-!Ambush zombies
-!Notes
-|-
-{}
+                if tmp_wave == wave_count:
+                    zombie_waves += '|}\n'
+                else:
+                    zombie_waves += '|-\n'
 
-==Gallery==
-<br />{{LevelsA}}
-""".format(level_name, full_level_name, level_name, gravestone_count, flag_count,
-           ' '.join(zmb_set) + ' {{P|Flag Zombie|2}}', before_level, after_level, no_numb_lvl_name, level_name,
-           final_string)
+            else:
+                tmp = sorted_list[i][::2]
+                tmp = ' '.join(tmp)
 
-    f.final_click(final_string)
+                zombie_waves = re.sub('\\|None(\\n\\|(<br />)*(Flag)*\n\\|[-}])(?![\\S\\s]+\\|None\n\\|(<br />)*('
+                                      'Flag)*\n\\|[-}])', f'|{tmp}\\1', zombie_waves)
+
+    zombies = set()
+    for item in range(len(sorted_list)):
+        if item % 2 == 0:
+            for i in sorted_list[item - 1][::2]:
+                zombies.add(re.sub('<sup>\\d</sup>', '', i))
+
+    zombies = '{{P|Flag Zombie|2}} ' + ''.join(zombies)
+
+    final_string = template.format(name=name, before_name=before_name, after_name=after_name, zombies=zombies,
+                                   plants=plants, name_number=name_number, num_items_on_map=num_items_on_map,
+                                   flags=flags, waves_per_flag=waves_per_flag, wave_count=wave_count,
+                                   zombie_waves=zombie_waves, first_reward=first_reward, replay_reward=replay_reward)
+
+    link = link.format(name=name, before_name=before_name, after_name=after_name, zombies=zombies,
+                       plants=plants, name_number=name_number, num_items_on_map=num_items_on_map,
+                       flags=flags, waves_per_flag=waves_per_flag, wave_count=wave_count,
+                       zombie_waves=zombie_waves, first_reward=first_reward, replay_reward=replay_reward)
+
+    r = Tk()
+    r.withdraw()
+    r.clipboard_clear()
+    r.clipboard_append(final_string)
+    r.update()
+    fun.openUrl(link)
+
+    return True
 
 
 if __name__ == '__main__':
-    f.show_window()
+    fun.show_window()
--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-import json
 import re
 import webbrowser
 from tkinter.messagebox import showinfo
@@ -8,9 +7,9 @@ import pyautogui
 from sys import platform
 from tkinter import *
 from tkinter import ttk
-import WikiToolsA as a
-import WikiToolsB as b
+import WikiTools as wt
 from tkinter import filedialog as fd
+import linecache
 
 width, height = pyautogui.size()
 pyautogui.PAUSE = 1
@@ -56,34 +55,32 @@ def openUrl(fin_url: str):
 
 
 def tmp_list(new_waves: list):
-    tmp_list = []
+    tmp_list_val = []
     for index in range(len(new_waves)):
         if index % 2 == 0:
             for ind in range(len(new_waves[index - 1])):
                 if ind % 2 == 1:
-                    tmp_list.append(new_waves[index - 1][ind - 1])
-    return tmp_list
+                    tmp_list_val.append(new_waves[index - 1][ind - 1])
+    return tmp_list_val
 
 
+# noinspection PyTypeChecker
 def show_window():
     file_type = [('json files', '*.json')]
 
-    def convert():
-        value = selected.get()
+    def start_convert():
         if str(root.file) != 'PY_VAR0':
             convert_button.configure(text='Converting...')
-            if value == 'Alpha':
-                a.main(str(root.file))
-            elif value == 'Beta':
-                b.main(str(root.file))
+            wt.convert(root.file)
+            convert_button.configure(text='Converted!')
         else:
-            showinfo(title='Warning', message='Please select a file')
+            showinfo(title='Error', message='Please select a file')
 
     root = Tk()
     root.file = StringVar(None, 'Select')
     root.title("WikiTools")
-    root.geometry("350x150")
-    # root.resizable(False, False)
+    root.geometry("300x150")
+    root.resizable(False, False)
     root.eval('tk::PlaceWindow . center')
 
     def select_file():
@@ -99,26 +96,26 @@ def show_window():
 
     button = ttk.Button(mainframe, text=re.sub('[\s\S]+/', '', str(root.file.get())), command=select_file)
     button.grid(column=2, row=1, sticky=(W, E))
-    convert_button = ttk.Button(mainframe, text="Convert", command=convert)
+    convert_button = ttk.Button(mainframe, text="Convert", command=start_convert)
     convert_button.grid(column=2, row=2, sticky=W)
 
     ttk.Label(mainframe, text="File path:").grid(column=1, row=1, sticky=W)
-
-    selected = StringVar(None, 'Alpha')
-    r1 = ttk.Radiobutton(mainframe, text='Alpha', value='Alpha', variable=selected)
-    r1.grid(column=3, row=1, sticky=W)
-    r2 = ttk.Radiobutton(mainframe, text='Beta', value='Beta', variable=selected)
-    r2.grid(column=3, row=2, sticky=W)
 
     ttk.Button(mainframe2, text="Settings", command=setting).grid(column=1, row=1, sticky=W)
 
     root.mainloop()
 
 
+# noinspection PyTypeChecker
 def setting():
+    file = open('template.txt', 'r').readlines()
+    template = ''.join(open('template.txt', 'r').readlines()[7:])
+    link_saved = ''.join(open('template.txt', 'r').readlines()[1:2])
+    template_values = re.sub(r'# ', '', ''.join(open('template.txt', 'r').readlines()[3:6]))
+
     root = Tk()
     root.title("Setup")
-    root.geometry("470x200")
+    root.geometry("550x550")
     root.resizable(False, False)
     root.eval('tk::PlaceWindow . center')
 
@@ -129,24 +126,45 @@ def setting():
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
 
-    ttk.Label(mainframe, text="Wiki edit link, use \"{}\" for level name").grid(column=1, row=1, sticky=W)
-    link = ttk.Entry(mainframe)
-    link.grid(column=1, row=3, sticky=(W, E))
-    ttk.Label(mainframe, text="Example: \"https://project-eclise.fandom.com/wiki/{}(Alpha)?action=edit\"").grid(column=1, row=2, sticky=W)
+    ttk.Label(mainframe, text="Wiki edit link, use \"{name_number}\" for level name").grid(column=1, row=1, sticky=W)
+    ttk.Label(mainframe, text="Example: \"https://project-eclise.fandom.com/wiki/{name_number}_(Alpha)?action=edit\"")\
+        .grid(column=1, row=2, sticky=W)
 
-    # ttk.Label(mainframe, text="").grid(column=1, row=4, sticky=W)
-    # ttk.Label(mainframe, text="Modifiers (like Alpha):").grid(column=1, row=5, sticky=W)
-    # modifiers = ttk.Entry(mainframe)
-    # modifiers.grid(column=1, row=6, sticky=(W, E))
+    link_val = StringVar(root, value=link_saved)
+    link = ttk.Entry(mainframe, textvariable=link_val)
+    link.grid(column=1, row=3, sticky=(W, E))
+
+    label_text = "Usable " + template_values
+    ttk.Label(mainframe, text="\nWiki template.").grid(column=1, row=4, sticky=W)
+    ttk.Label(mainframe, text='Use two curly brackets for one, use value inside curly brackets to add the value.')\
+        .grid(column=1, row=5, sticky=W)
+    ttk.Label(mainframe, text=label_text).grid(column=1, row=6, sticky=W)
+
+    text = Text(mainframe)
+    text.grid(column=1, row=7, sticky=(W, E))
+    text.insert('1.0', template)
 
     def save_setup(link1):
-        with open('data.json', 'w') as f:
-            json.dump({'link': link1}, f)
+        with open('template.txt', 'w') as f:
+            file[1] = link1
+            ''.join(file)
+            file[7:] = text.get('1.0', END)
+            f.writelines(file)
         root.destroy()
 
     ttk.Button(mainframe2, text="Save", command=lambda: save_setup(link.get())).grid(column=1, row=1, sticky=W)
 
     root.mainloop()
+
+
+def convert(string: str) -> str:
+    zombies_convert = open('convert.txt', 'r')
+
+    for line in range(len([line.strip("\n") for line in zombies_convert if line != "\n"])):
+        zombie = re.sub('\n', '', linecache.getline('convert.txt', line + 1)).split(': ')
+        string = re.sub(str(zombie[0]), str(zombie[1]), string)
+
+    return string
 
 
 if __name__ == '__main__':
